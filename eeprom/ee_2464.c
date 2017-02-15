32ece1b0cd7a545dd03bced33e30945086014417
James Jarvis <jj@aprsworld.com>
2017-02-15 23:56:42 +0000

    Reading pretty much implemented

--- eeprom/ee_2464.c
+++ eeprom/ee_2464.c
@@ -24,6 +24,9 @@ int main(int argc, char **argv) {
 	char ramSetBuffer[57];
 	char dateSetBuffer[20]; 
 	int dumpRead; 
+	int startAddress;
+	int nBytes;
+	char *inFilename, *outFilename;
 
 	/* I2C stuff */
 	char i2cDevice[64];	/* I2C device name */
@@ -44,6 +47,10 @@ int main(int argc, char **argv) {
 	memset(ramSetBuffer, 0, sizeof(ramSetBuffer));
 	memset(dateSetBuffer, 0, sizeof(dateSetBuffer));
 	dumpRead=0;
+	startAddress=0;
+	nBytes=8192;
+	inFilename=outFilename=NULL;
+
 	strcpy(i2cDevice,"/dev/i2c-1"); /* Raspberry PI normal user accessible I2C bus */
 	i2cAddress=0x50; 		/* 0b1010(A2)(A1)(A0) */
 
@@ -51,10 +58,14 @@ int main(int argc, char **argv) {
 		int this_option_optind = optind ? optind : 1;
 		int option_index = 0;
 		static struct option long_options[] = {
-		        {"dump",        no_argument,       0, 'd' },
-		        {"i2c-device",  required_argument, 0, 'i' },
-		        {"i2c-address", required_argument, 0, 'a' },
-		        {0,          0,                 0,  0 }
+		        {"read",           required_argument, 0, 'r' },
+		        {"write",          required_argument, 0, 'w' },
+		        {"dump",           no_argument,       0, 'd' },
+		        {"start-address",  required_argument, 0, 's' },
+		        {"n-bytes",        required_argument, 0, 'n' },
+		        {"i2c-device",     required_argument, 0, 'i' },
+		        {"i2c-address",    required_argument, 0, 'a' },
+		        {0,                0,                 0,  0 }
 		};
 
 		c = getopt_long(argc, argv, "", long_options, &option_index);
@@ -63,6 +74,26 @@ int main(int argc, char **argv) {
 			break;
 
 		switch (c) {
+			case 's':
+				startAddress=atoi(optarg);
+				if ( startAddress<0 || startAddress>8191 ) {
+					fprintf(stderr,"# start address out of range (0 to 8191)\n# Exiting...\n");
+					exit(1);
+				} 
+				break;
+			case 'n':
+				nBytes=atoi(optarg);
+				if ( nBytes<1 || nBytes>8192 ) {
+					fprintf(stderr,"# number of bytes out of range (1 to 8192)\n# Exiting...\n");
+					exit(1);
+				} 
+				break;
+			case 'w':
+				inFilename=optarg;
+				break;
+			case 'r':
+				outFilename=optarg;
+				break;
 			case 'a':
 				sscanf(optarg,"%x",&i2cAddress);
 				break;
@@ -80,6 +111,12 @@ int main(int argc, char **argv) {
 	fprintf(stderr,"# using I2C device %s\n",i2cDevice);
 	fprintf(stderr,"# using I2C device address of 0x%02X\n",i2cAddress);
 
+	if ( NULL != inFilename ) {
+		fprintf(stderr,"# input file: %s\n",inFilename);
+	}
+
+	fprintf(stderr,"# start address: %d\n",startAddress);
+	fprintf(stderr,"# bytes to read / write: %d\n",nBytes);
 
 
 	/* Open I2C bus */
@@ -98,8 +135,8 @@ int main(int argc, char **argv) {
 
 	txBuffer[0]=0x00;
 	txBuffer[1]=0x00;
-	sprintf(txBuffer+2,"hello!");
-	opResult = write(i2cHandle, txBuffer, 8);
+	sprintf(txBuffer+2,"short message");
+	opResult = write(i2cHandle, txBuffer, strlen(txBuffer+2)+2);
 	fprintf(stderr,"opResult=%d\n",opResult);
 	sleep(1);
 
@@ -160,13 +197,53 @@ int main(int argc, char **argv) {
 		memset(rxBuffer, 0, sizeof(rxBuffer));
 		opResult = read(i2cHandle, rxBuffer, sizeof(rxBuffer));
 
-		if ( dumpRead) {
-			fprintf(stderr,"# Dump from EEPROM\n");
-			for ( i=0 ; i<sizeof(rxBuffer) ; i++ ) {
-				putchar(rxBuffer[i]);
-			}
+		fprintf(stderr,"# Dump from EEPROM\n");
+		for ( i=0 ; i<sizeof(rxBuffer) ; i++ ) {
+			putchar(rxBuffer[i]);
+		}
+	} else if ( NULL != outFilename ) {
+		fprintf(stderr,"# outfile file: %s\n",outFilename);
+
+		/* open output file */
+		FILE *fp=fopen(outFilename,"w");
+		if ( NULL == fp ) {
+			fprintf(stderr,"# Error opening output file in write mode.\n# %s\n# Exiting...\n",strerror(errno));
+			exit(1);
+		}
+
+		/* address high byte */
+		txBuffer[0] = ((startAddress>>8) & 0b00011111);
+		/* address low byte */
+		txBuffer[1] = (startAddress & 0b11111111);
+
+		/* write read address */
+		opResult = write(i2cHandle, txBuffer, 2);
+		if (opResult != 2) {
+			fprintf(stderr,"# No ACK! Exiting...\n");
+			exit(2);
+		}
+
+		/*  read buffer length */
+		memset(rxBuffer, 0, sizeof(rxBuffer));
+		opResult = read(i2cHandle, rxBuffer, nBytes);
+
+		/* write to file */
+		int written = fwrite(rxBuffer,1,nBytes,fp);
+		if ( written != nBytes ) {
+			fprintf(stderr,"# Error writing EEPROM data to output file. %d bytes written.\n# Exiting...\n",written);
+			exit(1);
+
+		}
+		
+
+		/* close output file */
+		if ( 0 != fclose(fp) ) {
+			fprintf(stderr,"# Error closing output file.\n# %s\n# Exiting...\n",strerror(errno));
+			exit(1);
 		}
 	}
+
+
 	if ( -1 == close(i2cHandle) ) {
 		fprintf(stderr,"# Error closing I2C device.\n# %s\n# Exiting...\n",strerror(errno));
 		exit(1);
