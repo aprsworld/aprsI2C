581dd866ae56657c5443a8e2fca8cfe9e811ca0d
James Jarvis <jj@aprsworld.com>
2017-02-16 16:28:31 +0000

    working on big writes

--- eeprom/ee_2464.c
+++ eeprom/ee_2464.c
@@ -13,6 +13,8 @@
 extern char *optarg;
 extern int optind, opterr, optopt;
 
+/* number of acknowledgement cycles to poll on write for */
+#define TIMEOUT_NTRIES 50
 
 
 int main(int argc, char **argv) {
@@ -21,12 +23,11 @@ int main(int argc, char **argv) {
 	int digit_optind = 0;
 
 	/* program flow */
-	char ramSetBuffer[57];
-	char dateSetBuffer[20]; 
 	int dumpRead; 
 	int startAddress;
 	int nBytes;
 	char *inFilename, *outFilename;
+	int breakOnNull;
 
 	/* I2C stuff */
 	char i2cDevice[64];	/* I2C device name */
@@ -38,18 +39,17 @@ int main(int argc, char **argv) {
 	int opResult = 0;	/* for error checking of operations */
 
 	/* EEPROM stuff */
-	int i;
+	int i, nTries;
 
 
 	fprintf(stderr,"# ee_2464 24AA64 / 24LC64 EEPROM I2C utility\n");
 
 	/* defaults and command line arguments */
-	memset(ramSetBuffer, 0, sizeof(ramSetBuffer));
-	memset(dateSetBuffer, 0, sizeof(dateSetBuffer));
 	dumpRead=0;
 	startAddress=0;
 	nBytes=8192;
 	inFilename=outFilename=NULL;
+	breakOnNull=0;
 
 	strcpy(i2cDevice,"/dev/i2c-1"); /* Raspberry PI normal user accessible I2C bus */
 	i2cAddress=0x50; 		/* 0b1010(A2)(A1)(A0) */
@@ -61,6 +61,7 @@ int main(int argc, char **argv) {
 		        {"read",           required_argument, 0, 'r' },
 		        {"write",          required_argument, 0, 'w' },
 		        {"dump",           no_argument,       0, 'd' },
+		        {"string",         no_argument,       0, 'b' },
 		        {"start-address",  required_argument, 0, 's' },
 		        {"n-bytes",        required_argument, 0, 'n' },
 		        {"i2c-device",     required_argument, 0, 'i' },
@@ -74,6 +75,9 @@ int main(int argc, char **argv) {
 			break;
 
 		switch (c) {
+			case 'b':
+				breakOnNull=1;
+				break;
 			case 's':
 				startAddress=atoi(optarg);
 				if ( startAddress<0 || startAddress>8191 ) {
@@ -133,43 +137,81 @@ int main(int argc, char **argv) {
 	/* address of device we will be working with */
 	opResult = ioctl(i2cHandle, I2C_SLAVE, i2cAddress);
 
-	txBuffer[0]=0x00;
-	txBuffer[1]=0x00;
-	sprintf(txBuffer+2,"short message");
-	opResult = write(i2cHandle, txBuffer, strlen(txBuffer+2)+2);
-	fprintf(stderr,"opResult=%d\n",opResult);
-	sleep(1);
 
-#if 0
-	/* do sets first */
-	if ( strlen(dateSetBuffer) ) {
-		fprintf(stderr,"# Attempting to set date with '%s'\n",dateSetBuffer);
-
-		if ( 0 != ds1307_build_date_from_string(dateSetBuffer, txBuffer+1) ) {
-			fprintf(stderr,"# invalid date format or value. Exiting...\n");
-			exit(3);
+	/* write operation if needed */
+	if ( NULL != inFilename ) {
+		fprintf(stderr,"# input file: %s\n",inFilename);
+		if ( breakOnNull ) {
+			fprintf(stderr,"# write string mode (stopping at first null)\n");
+		} else {
+		 	fprintf(stderr,"# write binary mode\n");
 		}
 
-		txBuffer[0]=0x00; /* address of clock registers */
-		/* txBuffer should now be filled with address of clock register and 8 bytes of clock data. Now we can write. */
+		/* open input file */
+		FILE *fp=fopen(inFilename,"r");
+		if ( NULL == fp ) {
+			fprintf(stderr,"# Error opening input file in read mode.\n# %s\n# Exiting...\n",strerror(errno));
+			exit(1);
+		}
 
-		opResult = write(i2cHandle, txBuffer, 9);
-		if (opResult != 9) {
-			fprintf(stderr,"# Error writing date. write() returned %d instead of 9. Exiting...\n",opResult);
-			exit(2);
+		while (1) { 
+			/* EEPROM write is in 32 byte or less chunks. After writing, we must poll for ack to know that write is done */
+
+			/* read up to 32 bytes */
+			for ( i=2 ; i<32+2 ; i++ ) {
+				txBuffer[i] = fgetc(fp);
+
+				if ( EOF == txBuffer[i] ) {
+					break;
+				}
+
+				if ( breakOnNull && '\0' == txBuffer[i] ) {
+					break;
+				}
+			}
+			fprintf(stderr,"# i=%d\n",i);
+
+	
+
+			/* address high byte */
+			txBuffer[0] = ((startAddress>>8) & 0b00011111);
+			/* address low byte */
+			txBuffer[1] = (startAddress & 0b11111111);
+
+			int j;
+			for ( j=0 ; j<32+2 ; j++ ) {
+				fprintf(stderr,"# txBuffer[%d] = 0x%02X\n",j,txBuffer[j]);
+
+			}
+
+			/* fixme */
+			break;
+		}
+
+		/* close output file */
+		if ( 0 != fclose(fp) ) {
+			fprintf(stderr,"# Error closing input file.\n# %s\n# Exiting...\n",strerror(errno));
+			exit(1);
 		}
 	}
 
-	if ( strlen(ramSetBuffer) ) {
-		fprintf(stderr,"# writing up to 56 bytes of '%s' (padded with \\0 if less than 56 bytes) to RAM\n",ramSetBuffer);
+#if 0
+	txBuffer[0]=0x00;
+	txBuffer[1]=0x00;
+	sprintf(txBuffer+2,"short message...");
+	opResult = write(i2cHandle, txBuffer, strlen(txBuffer+2)+3); /* +3 is for two address bytes + 1 null */
+	fprintf(stderr,"opResult=%d\n",opResult);
 
-		txBuffer[0]=0x08; /* address of clock registers */
-		memcpy(txBuffer+1,ramSetBuffer,56);
-		/* txBuffer should now be filled with address of first RAM register and 56 bytes of ram data. Now we can write. */
+	/* poll for acknowledgement so we can write the next page */
+	for ( nTries=0 ; ; nTries++ ) {
+		opResult = write(i2cHandle,txBuffer,0);
+		fprintf(stderr,"poll nTries=%d opResult=%d\n",nTries,opResult);
 
-		opResult = write(i2cHandle, txBuffer, 57);
-		if (opResult != 57) {
-			fprintf(stderr,"# Error writing RAM data. write() returned %d instead of 57. Exiting...\n",opResult);
+		if ( opResult != -1 ) 
+			break;
+
+		if ( nTries >= TIMEOUT_NTRIES ) {
+			fprintf(stderr,"# Timeout while polling for write acknowledgement! Exiting...\n");
 			exit(2);
 		}
 	}
@@ -179,8 +221,6 @@ int main(int argc, char **argv) {
 	/* do any requested reading after sets */
 	if ( dumpRead ) {
 
-		/* control byte */
-//		txBuffer[0] = 0b10100000; 
 		/* address high byte */
 		txBuffer[0] = 0x00;
 		/* address low byte */
@@ -202,7 +242,12 @@ int main(int argc, char **argv) {
 			putchar(rxBuffer[i]);
 		}
 	} else if ( NULL != outFilename ) {
-		fprintf(stderr,"# outfile file: %s\n",outFilename);
+		fprintf(stderr,"# output file: %s\n",outFilename);
+		if ( breakOnNull ) {
+			fprintf(stderr,"# read string mode (stopping before first null)\n");
+		} else {
+		 	fprintf(stderr,"# read binary mode\n");
+		}
 
 		/* open output file */
 		FILE *fp=fopen(outFilename,"w");
@@ -228,11 +273,19 @@ int main(int argc, char **argv) {
 		opResult = read(i2cHandle, rxBuffer, nBytes);
 
 		/* write to file */
-		int written = fwrite(rxBuffer,1,nBytes,fp);
-		if ( written != nBytes ) {
-			fprintf(stderr,"# Error writing EEPROM data to output file. %d bytes written.\n# Exiting...\n",written);
-			exit(1);
-
+		if ( breakOnNull ) {
+			for ( i=0 ; i<nBytes && '\0' != rxBuffer[i] ; i++ ) {
+				if ( EOF == fputc(rxBuffer[i],fp) ) {
+					fprintf(stderr,"# Error writing EEPROM data to output file. %d bytes written.\n# Exiting...\n",i-1);
+					exit(1);
+				}
+			}
+		} else {
+			int written = fwrite(rxBuffer,1,nBytes,fp);
+			if ( written != nBytes ) {
+				fprintf(stderr,"# Error writing EEPROM data to output file. %d bytes written.\n# Exiting...\n",written);
+				exit(1);
+			}
 		}
 		
 
