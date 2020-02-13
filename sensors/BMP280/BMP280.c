55b668bac6e91b86e564acef216cb6fd3c8f2dbb
James Jarvis <jj@aprsworld.com>
2020-02-13 12:53:52 +0000

    Separating bmp280 init from read

--- sensors/BMP280/BMP280.c
+++ sensors/BMP280/BMP280.c
@@ -36,12 +36,33 @@ static uint64_t microtime(void) {
 	gettimeofday(&time, NULL); 
 	return ((uint64_t)time.tv_sec * 1000000) + time.tv_usec;
 }
-static void sample(int i2cHandle) {
+
+
+int bmp280_make_int(uint8_t msb, uint8_t lsb, int sign_extend) {
+	int i;
+
+	i = msb * 256 + lsb;
+
+	if ( sign_extend && i > 32767 ) {
+		i -= 65536;
+	}
+
+	return i;
+}
+
+void bmp280_init(int i2cHandle) {
+
+}
+
+
+/* read bmp280 device that has been previously configured */
+static void bmp280_sample(int i2cHandle) {
 	struct timeval time;
 	char buffer[32];
 	char buff1[32];
 	char timestamp[32];
 	struct tm *now;
+	int opResult;
 
 	gettimeofday(&time, NULL); 
 	/* JSON stuff */
@@ -49,78 +70,71 @@ static void sample(int i2cHandle) {
 
 	// Read 24 bytes of data from address(0x88)
 	uint8_t reg[1] = {0x88};
-	write(i2cHandle, reg, 1);
+	opResult = write(i2cHandle, reg, 1);
+	if (opResult != 1) {
+		fprintf(stderr,"# I2C write error in address set. No ACK! Exiting...\n");
+		exit(2);
+	}
+
+
 	uint8_t data[24] = {0};
-	if(read(i2cHandle, data, 24) != 24) {
-		printf("Error : Input/output Error \n");
+	if ( read(i2cHandle, data, 24) != 24 ) {
+		fprintf(stderr,"# I2C read error. Exiting...\n");
 		exit(1);
 	}
-	// Convert the data
-	// temp coefficents
-	int dig_T1 = data[1] * 256 + data[0];
-	int dig_T2 = data[3] * 256 + data[2];
-	if(dig_T2 > 32767) {
-		dig_T2 -= 65536;
-	}
-	int dig_T3 = data[5] * 256 + data[4];
-	if(dig_T3 > 32767) {
-		dig_T3 -= 65536;
-	}
-	// pressure coefficents
-	int dig_P1 = data[7] * 256 + data[6];
-	int dig_P2  = data[9] * 256 + data[8];
-	if(dig_P2 > 32767) {
-		dig_P2 -= 65536;
-	}
-	int dig_P3 = data[11]* 256 + data[10];
-	if(dig_P3 > 32767) {
-		dig_P3 -= 65536;
-	}
-	int dig_P4 = data[13]* 256 + data[12];
-	if(dig_P4 > 32767) {
-		dig_P4 -= 65536;
-	}
-	int dig_P5 = data[15]* 256 + data[14];
-	if(dig_P5 > 32767) {
-		dig_P5 -= 65536;
-	}
-	int dig_P6 = data[17]* 256 + data[16];
-	if(dig_P6 > 32767) {
-		dig_P6 -= 65536;
-	}
-	int dig_P7 = data[19]* 256 + data[18];
-	if(dig_P7 > 32767) {
-		dig_P7 -= 65536;
-	}
-	int dig_P8 = data[21]* 256 + data[20];
-	if(dig_P8 > 32767) {
-		dig_P8 -= 65536;
-	}
-	int dig_P9 = data[23]* 256 + data[22];
-	if(dig_P9 > 32767) {
-		dig_P9 -= 65536;
-	}
+
+	/* convert data to real units */
+	/* temperature */
+	int dig_T1, dig_T2, dig_T3;
+	dig_T1 = bmp280_make_int(data[1],data[0],0); /* unsigned */
+	dig_T2 = bmp280_make_int(data[3],data[2],1); /* signed */
+	dig_T3 = bmp280_make_int(data[5],data[4],1); 
+	
+	/* pressure */
+	int dig_P1, dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
+	dig_P1 = bmp280_make_int(data[7],data[6],0); 
+	dig_P2 = bmp280_make_int(data[9],data[8],1); 
+	dig_P3 = bmp280_make_int(data[11],data[10],1); 
+	dig_P4 = bmp280_make_int(data[13],data[12],1); 
+	dig_P5 = bmp280_make_int(data[15],data[14],1); 
+	dig_P6 = bmp280_make_int(data[17],data[16],1); 
+	dig_P7 = bmp280_make_int(data[19],data[18],1); 
+	dig_P8 = bmp280_make_int(data[21],data[20],1); 
+	dig_P9 = bmp280_make_int(data[23],data[22],1); 
+
 		
 	// Select control measurement register(0xF4)
 	// Normal mode, temp and pressure over sampling rate = 1(0x27)
 	uint8_t config[2] = {0};
 	config[0] = 0xF4;
 	config[1] = 0x27;
-	write(i2cHandle, config, 2);
+	opResult = write(i2cHandle, config, 2);
+	if (opResult != 2) {
+		fprintf(stderr,"# I2C write error in setting mode. No ACK! Exiting...\n");
+		exit(2);
+	}
 	
 	// Select config register(0xF5)
 	// Stand_by time = 1000 ms(0xA0)
 	config[0] = 0xF5;
 	config[1] = 0xA0;
-	write(i2cHandle, config, 2);
+	opResult = write(i2cHandle, config, 2);
+	if (opResult != 2) {
+		fprintf(stderr,"# I2C write error in setting standby time. No ACK! Exiting...\n");
+		exit(2);
+	}
 	// sleep(1);
 	
 	// Read 8 bytes of data from register(0xF7)
 	// pressure msb1, pressure msb, pressure lsb, temp msb1, temp msb, temp lsb, humidity lsb, humidity msb
 	reg[0] = 0xF7;
-	write(i2cHandle, reg, 1);
-	if(read(i2cHandle, data, 8) != 8) {
-		printf("Error : Input/output Error \n");
+	opResult = write(i2cHandle, reg, 1);
+	if (opResult != 1) {
+		fprintf(stderr,"# I2C write error. No ACK! Exiting...\n");
+		exit(2);
+	}
+	if ( read(i2cHandle, data, 8) != 8 ) {
+		fprintf(stderr, "# I2C read error. Exiting...\n");
 		exit(1);
 	}
 	
@@ -266,11 +280,16 @@ int main(int argc, char **argv) {
 
 
 
-//int BMP280_make_int(
 	fprintf(stderr,"# samplingInterval = %d mSeconds\n",samplingInterval);
 
+	/* I2C running, now initialize / configure hardware */
+	bmp280_init(i2cHandle);
+
+
 	while ( 1 ) {
-		sample(i2cHandle);
+		bmp280_sample(i2cHandle);
+		/* TODO: more periodic sampling */
+		/* TODO: possible solution discussed at https://qnaplus.com/implement-periodic-timer-linux/ */
 		usleep(1000*samplingInterval);
 	}
 
